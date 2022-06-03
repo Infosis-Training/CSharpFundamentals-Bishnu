7db375e9a9b89f2f96bccb874fc071dc496c2c3c
Bishnu Rawal <Bishnu.Rawal@cotiviti.com>
2022-06-03 07:42:27 +0545

    Add min method

--- ConsoleApp/Excercise Solution/Exercise2.cs
+++ ConsoleApp/Excercise Solution/Exercise2.cs
@@ -17,6 +17,23 @@ class Exercise2
                return z; 
         }
     }
+    public int GetMin(int x, int y, int z)
+    {
+        if(x < y)
+        {
+            if(x < z)
+                return x;
+            else
+                return z;
+        }
+        else
+        {
+            if(y < z)
+                return y;
+            else
+               return z; 
+        }
+    }
 
     internal double Multiply(double x, double y, double z = 1) => x * y * z;
 
