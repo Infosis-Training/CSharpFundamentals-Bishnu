f5f882620049e84fcc9b9be8c79aa132e2f3436f
Bishnu Rawal <Bishnu.Rawal@cotiviti.com>
2022-06-09 07:54:20 +0545

    Bind movie list to view

--- Web Development/MovieManagement/MovieManagement/Controllers/MovieController.cs
+++ Web Development/MovieManagement/MovieManagement/Controllers/MovieController.cs
@@ -1,12 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MovieManagement.Models;
+using System.Collections.Generic;
 
 namespace MovieManagement.Controllers
 {
     public class MovieController : Controller
     {
         public IActionResult Index()
-        {
-            return View();
+        {            
+            Movie movie1 = new()
+            {
+                Name = "3 Idiots",
+                Code = "A1BC56",
+                Genre = "Comedy",
+                Description = "Movie based on real story",
+                LengthInMin = 178,
+                ReleaseDate = DateTime.Parse("2015-01-01")
+            };
+            
+            Movie movie2 = new()
+            {
+                Name = "KGF",
+                Code = "D1BC897",
+                Genre = "Action Drama",
+                Description = "Movie based on gold smuggling",
+                LengthInMin = 167,
+                ReleaseDate = DateTime.Parse("2021-04-12")
+            };
+
+            List<Movie> movies = new() { movie1, movie2 };
+
+            return View(movies);
         }
     }
 }
