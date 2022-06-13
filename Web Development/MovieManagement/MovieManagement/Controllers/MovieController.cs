cba12ba8e6b1a31f35f636c8dc725ae019db082a
Bishnu Rawal <Bishnu.Rawal@cotiviti.com>
2022-06-13 09:11:22 +0545

    Add and fetch from db

--- Web Development/MovieManagement/MovieManagement/Controllers/MovieController.cs
+++ Web Development/MovieManagement/MovieManagement/Controllers/MovieController.cs
@@ -1,53 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MovieManagement.Data;
 using MovieManagement.Models;
-using System.Collections.Generic;
 
 namespace MovieManagement.Controllers
 {
     public class MovieController : Controller
     {
+        private readonly MovieManagementDb _db;
+
+        public MovieController(MovieManagementDb db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
-        {            
-            Movie movie1 = new()
-            {
-                Name = "3 Idiots",
-                Code = "A1BC56",
-                Genre = "Comedy",
-                Description = "Movie based on real story",
-                LengthInMin = 178,
-                ReleaseDate = DateTime.Parse("2015-01-01")
-            };
-            
-            Movie movie2 = new()
-            {
-                Name = "KGF",
-                Code = "D1BC897",
-                Genre = "Action Drama",
-                Description = "Movie based on gold smuggling",
-                LengthInMin = 167,
-                ReleaseDate = DateTime.Parse("2021-04-12")
-            };
-            Movie movie3 = new()
-            {
-                Name = "KGF",
-                Code = "D1BC897",
-                Genre = "Action Drama",
-                Description = "Movie based on gold smuggling",
-                LengthInMin = 167,
-                ReleaseDate = DateTime.Parse("2021-04-12")
-            };
-            Movie movie4 = new()
-            {
-                Name = "KGF",
-                Code = "D1BC897",
-                Genre = "Action Drama",
-                Description = "Movie based on gold smuggling",
-                LengthInMin = 167,
-                ReleaseDate = DateTime.Parse("2021-04-12")
-            };
-
-            List<Movie> movies = new() { movie1, movie2, movie3, movie4 };
-
+        {
+            var movies = _db.Movies.ToList();
             return View(movies);
         }
 
@@ -60,7 +28,10 @@ namespace MovieManagement.Controllers
         [HttpPost]
         public IActionResult Add(Movie movie)
         {
-            //Do something with movie object
+            movie.Code = Guid.NewGuid().ToString();
+
+            _db.Movies.Add(movie);
+            _db.SaveChanges();
 
             return RedirectToAction(nameof(Index));
         }
